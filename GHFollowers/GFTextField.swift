0c98bc73bb360ebf0115c632d2157e664ec78bd6
Orlando Vargas <Orlandov87@live.com>
2020-08-20 22:07:47 -0400

    Remove a redundant line of code.

--- GHFollowers/GFTextField.swift
+++ GHFollowers/GFTextField.swift
@@ -35,7 +35,6 @@ class GFTextField: UITextField {
         
         backgroundColor = .tertiarySystemBackground
         autocorrectionType = .no
-        keyboardType = .default
         returnKeyType = .go
         
         placeholder = "Enter a user name"
